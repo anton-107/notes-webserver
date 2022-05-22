13aac7d919d0f3d98fe8e3869e940946a5c081ff
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-05-22 16:07:49 +0200

    wip: say hello to username

--- src/notes-webserver.ts
+++ src/notes-webserver.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as cookieParser from "cookie-parser";
 import { Express } from "express";
 
 export class NotesWebserver {
@@ -7,7 +8,9 @@ export class NotesWebserver {
 
   constructor() {
     this.app = express();
+    this.app.use(cookieParser());
     this.app.get("/home", (req, res) => {
+      console.log('RequestCookies: ', req.cookies);
       res.setHeader("Content-Type", "text/html; charset=utf-8");
       res.send("<h1>hello anonymous!</h1><a>Sign in</a>");
     });
--- test/webserver.test.ts
+++ test/webserver.test.ts
@@ -14,12 +14,22 @@ describe("Notes webserver", () => {
     server && server.stop();
   });
 
-  it("should render home page in html", async () => {
+  it("should render home page for an anonymous user in html", async () => {
     const testClient = new HttpClient();
     const response = await testClient.get(`http://localhost:${testPort}/home`);
     expect(response.getHeader("content-type")).toContain("text/html");
     expect(response.getBody()).toContain("hello anonymous!");
     expect(response.getBody()).toContain("Sign in");
     expect(response.getBody()).not.toContain("Sign out");
+  });
+  it("should render home page for a signed in user", async () => {
+    const userToken = 'sample-user-token:user1';
+    const testClient = new HttpClient();
+    testClient.addCookie(`Authentication=usertoken ${userToken}`);
+    const response = await testClient.get(`http://localhost:${testPort}/home`);
+    expect(response.getHeader("content-type")).toContain("text/html");
+    expect(response.getBody()).toContain("hello user1!");
+    expect(response.getBody()).not.toContain("Sign in");
+    expect(response.getBody()).toContain("Sign out");
   });
 });
