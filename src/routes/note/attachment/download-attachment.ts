f4c26b0d49dc5bf2c0754da5047bc87941a07e0a
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-12-21 19:48:26 +0100

    use NoteControllerBuilder in note attachments routes

--- src/routes/note/attachment/download-attachment.ts
+++ src/routes/note/attachment/download-attachment.ts
@@ -1,28 +1,19 @@
-import { noteControllerConfiguration } from "../../../configuration/configuration";
-import { NoteController } from "../../../controller/note/note-controller";
-import { parseCookie } from "../../../http/cookie-parser";
+import { NoteControllerBuilder } from "../../../controller/note/note-controller-builder";
 import {
   HttpRequest,
   HttpRequestHandler,
-  HttpResponse,
+  HttpResponse
 } from "../../../http/http";
 import { parseResponseType } from "../../../http/response-type-parser";
-import { NoteHtmlView } from "../../../views/note/note-html-view";
 
 export const downloadAttachmentHandler: HttpRequestHandler = async (
   request: HttpRequest
 ): Promise<HttpResponse> => {
-  const configuration = noteControllerConfiguration({});
-  return await new NoteController({
-    ...configuration,
-    entityView: new NoteHtmlView({
-      ...configuration,
-    }),
-    notebookID: null,
-    noteType: null,
-    authenticationToken: parseCookie(request.headers, "Authentication"),
+  const controller = NoteControllerBuilder.build({
+    headers: request.headers,
     responseType: parseResponseType(request.headers),
-  }).downloadAttachment(
+  });
+  return await controller.downloadAttachment(
     request.pathParameters.noteID,
     request.pathParameters.attachmentID
   );
--- src/routes/note/attachment/get-note-attachments.ts
+++ src/routes/note/attachment/get-note-attachments.ts
@@ -1,26 +1,17 @@
-import { noteControllerConfiguration } from "../../../configuration/configuration";
-import { NoteController } from "../../../controller/note/note-controller";
-import { parseCookie } from "../../../http/cookie-parser";
+import { NoteControllerBuilder } from "../../../controller/note/note-controller-builder";
 import {
   HttpRequest,
   HttpRequestHandler,
-  HttpResponse,
+  HttpResponse
 } from "../../../http/http";
 import { parseResponseType } from "../../../http/response-type-parser";
-import { NoteHtmlView } from "../../../views/note/note-html-view";
 
 export const getNoteAttachmentsHandler: HttpRequestHandler = async (
   request: HttpRequest
 ): Promise<HttpResponse> => {
-  const configuration = noteControllerConfiguration({});
-  return await new NoteController({
-    ...configuration,
-    entityView: new NoteHtmlView({
-      ...configuration,
-    }),
-    notebookID: null,
-    noteType: null,
-    authenticationToken: parseCookie(request.headers, "Authentication"),
+  const controller = NoteControllerBuilder.build({
+    headers: request.headers,
     responseType: parseResponseType(request.headers),
-  }).listAttachments(request.pathParameters.noteID);
+  });
+  return await controller.listAttachments(request.pathParameters.noteID);
 };
