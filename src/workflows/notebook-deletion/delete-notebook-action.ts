62f3dcedb2b3ba25cbcd51d5769c88bfec45d7e6
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-02-20 21:53:50 +0100

    implement delete notebook action in delete notebook workflow

--- src/workflows/notebook-deletion/delete-notebook-action.ts
+++ src/workflows/notebook-deletion/delete-notebook-action.ts
@@ -1,5 +1,31 @@
 import { notebookControllerConfiguration } from "../../configuration/configuration";
+import { Logger } from "../../logger/logger";
+import { NotebookStore } from "../../stores/notebook/notebook-store";
 import { NotebookDeletionOutput } from "./delete-all-notes-in-notebook-action";
+
+interface DeleteNotebookActionProperties {
+  logger: Logger;
+  notebookStore: NotebookStore;
+}
+
+export class DeleteNotebookAction {
+  constructor(private properties: DeleteNotebookActionProperties) {}
+  public async run(event: NotebookDeletionOutput): Promise<void> {
+    this.properties.logger.info(
+      "Running DeleteNotebookAction for the following notebook id: ",
+      { entityID: event.notebookID, username: event.owner }
+    );
+    await this.properties.notebookStore.deleteOne(
+      event.owner,
+      event.notebookID
+    );
+    this.properties.logger.info("Notebook deleted", {
+      entityID: event.notebookID,
+      username: event.owner,
+    });
+    return;
+  }
+}
 
 export async function deleteNotebook({
   Payload,
@@ -7,9 +33,9 @@ export async function deleteNotebook({
   Payload: NotebookDeletionOutput;
 }): Promise<void> {
   const configuration = notebookControllerConfiguration({});
-  configuration.logger.info(
-    "Running verifyNoNotesInNotebook for the following notebook id: ",
-    { entityID: Payload.notebookID, username: Payload.owner }
-  );
-  return;
+  const action = new DeleteNotebookAction({
+    logger: configuration.logger,
+    notebookStore: configuration.notebookStore,
+  });
+  return await action.run(Payload);
 }
---
+++ test/workflows/notebook-deletion/delete-notebook-action.test.ts
@@ -0,0 +1,39 @@
+import { LoggerBunyan } from "../../../src/logger/logger-bunyan";
+import { InMemoryNotebookStore } from "../../../src/stores/notebook/notebook-store";
+import {
+  deleteNotebook,
+  DeleteNotebookAction,
+} from "../../../src/workflows/notebook-deletion/delete-notebook-action";
+
+describe("deleteNotebook", () => {
+  it("throws an error if no notebook is found to be deleted", async () => {
+    await expect(async () => {
+      await deleteNotebook({
+        Payload: { notebookID: "notebook1", owner: "user1" },
+      });
+    }).rejects.toThrow("Notebook is not found");
+  });
+  it("deletes a notebook", async () => {
+    const notebookStore = new InMemoryNotebookStore();
+    notebookStore.add({
+      id: "notebook-1",
+      owner: "user1",
+      name: "My notebook",
+      sections: [],
+      tableColumns: [],
+      createdAt: "now",
+      updatedAt: "now",
+      status: "MARKED_FOR_DELETION",
+    });
+    const action = new DeleteNotebookAction({
+      logger: new LoggerBunyan(),
+      notebookStore: notebookStore,
+    });
+    await action.run({
+      notebookID: "notebook-1",
+      owner: "user1",
+    });
+    const notebooks = await notebookStore.listAll("user1");
+    expect(notebooks.length).toBe(0);
+  });
+});
