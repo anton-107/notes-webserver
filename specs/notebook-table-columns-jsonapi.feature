30a26111d39e7be7a3571c6921c0bdf94a779062
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-03-11 18:55:49 +0100

    specification for notebook-supported-columns json api operation

---
+++ specs/notebook-table-columns-jsonapi.feature
@@ -0,0 +1,14 @@
+Feature: Notebook table columns
+  Scenario: Listing a list of supported columns
+    
+    Background: Notebook table columns are taken from NotebookTableColumnsRegistry which is configured in the common configuration
+
+    Given web server is running
+    Given I am logged in as 'user1'/'1234'
+    When I visit /notebook-supported-columns page
+    When json response is loaded
+    Then json query 'columns[0].name' returns value 'Due date'
+    Then json query 'columns[1].name' returns value 'Start date'
+    Then json query 'columns[2].name' returns value 'End date'
+    Then json query 'columns[3].name' returns value 'Assignee'
+    Then json query 'columns[4].name' returns value 'Completed'
---
+++ specs/notebook-table-columns-jsonapi.test.ts
@@ -0,0 +1,52 @@
+import { defineFeature, loadFeature } from "jest-cucumber";
+
+import { TestScenario } from "./test-scenario";
+
+const feature = loadFeature("./specs/notebook-table-columns-jsonapi.feature");
+
+defineFeature(feature, (test) => {
+  const testScenario = new TestScenario(8148);
+  afterAll(() => testScenario.stopServer());
+  test("Listing a list of supported columns", ({ given, when, then }) => {
+    given("web server is running", () => testScenario.startServer());
+    given(
+      /^I am logged in as '([A-z0-9]+)'\/'([A-z0-9]+)'$/,
+      (user, password) => testScenario.loginAs(user, password)
+    );
+
+    when(
+      /^I visit \/([a-z{}\-/]+) page$/,
+      async (url) => await testScenario.loadPage(url)
+    );
+
+    when(
+      "json response is loaded",
+      async () => await testScenario.processJSON()
+    );
+
+    then(
+      /^json query '([^']+)' returns value '([^']+)'$/,
+      (query, fieldValue) => testScenario.checkJSONQuery(query, fieldValue)
+    );
+
+    then(
+      /^json query '([^']+)' returns value '([^']+)'$/,
+      (query, fieldValue) => testScenario.checkJSONQuery(query, fieldValue)
+    );
+
+    then(
+      /^json query '([^']+)' returns value '([^']+)'$/,
+      (query, fieldValue) => testScenario.checkJSONQuery(query, fieldValue)
+    );
+
+    then(
+      /^json query '([^']+)' returns value '([^']+)'$/,
+      (query, fieldValue) => testScenario.checkJSONQuery(query, fieldValue)
+    );
+
+    then(
+      /^json query '([^']+)' returns value '([^']+)'$/,
+      (query, fieldValue) => testScenario.checkJSONQuery(query, fieldValue)
+    );
+  });
+});
